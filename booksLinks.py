a33db57a35a9265df715c5dea3fa57d2c80c4b55
akfio <74294901+akfio@users.noreply.github.com>
2020-12-26 13:54:28 +0100

    Fonction de récupération des liens des livres

--- booksLinks.py
+++ booksLinks.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
-#url= 'http://books.toscrape.com/catalogue/category/books/poetry_23/index.html'
 url = 'http://books.toscrape.com/catalogue/category/books/young-adult_21/index.html'
-base_url = url.replace('index.html', '')
-lst = []
 
-def books_category_parse_page(url):
+
+
+def get_lst_products(url):
+    lst = []
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, 'html.parser')
     liens = soup.find('ol', {'class': 'row'}).find_all('article', {'class': 'product_pod'})
@@ -14,13 +15,14 @@ def books_category_parse_page(url):
         a = lien.find('a')
         link = 'http://books.toscrape.com/catalogue/' + a['href']
         links = link.replace('../../../', '')
-        print(links)
         lst.append(links)
-    if soup.find('li', {'class': 'next'}) != None :
+    if soup.find('li', {'class': 'next'}) is not None:
         np = soup.find('li', {'class': 'next'}).find('a')['href']
-        next_page_url = base_url + np
-        books_category_parse_page(next_page_url)
-    else:
-        ''
-
-books_category_parse_page(url)
+        a = urlparse(url).path
+        b = a.index('/', 26)
+        c = a[:b + 1]
+        next_page_url = 'http://books.toscrape.com' + c + np
+        data = get_lst_products(next_page_url)
+        for d in data:
+            lst.append(d)
+    return lst

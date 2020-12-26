bf1bb32bd01ea07d1f0d4f275ce98e9ac6265fda
akfio <74294901+akfio@users.noreply.github.com>
2020-12-26 13:37:08 +0100

    Ensemble des fonctions pour le process

--- books.py
+++ books.py
@@ -4,7 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-def liens_categories():
+def get_category_link():
+    lks = []
     url = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, 'html.parser')
@@ -13,24 +14,23 @@ def liens_categories():
         a = lien.find('a')
         link = 'http://books.toscrape.com/catalogue/category/' + a['href']
         links = link.replace('../', '')
-        print(links)
+        lks.append(links)
+    return lks
 
-def categories_names():
-    names = []
+
+def get_category_name():
+    noms_cat = []
     url = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, 'html.parser')
     noms = soup.find('div', {'class': 'side_categories'}).find('ul', {'class': 'nav nav-list'}).find('li').find('ul').find_all('li')
     for nom in noms:
         i = str.strip(nom.find('a').text)
-        names.append(i)
-    print(names)
+        noms_cat.append(i)
+    return noms_cat
 
-url = 'http://books.toscrape.com/catalogue/category/books/young-adult_21/index.html'
-base_url = url.replace('index.html', '')
-lst = []
-
-def books_category_parse_page(url):
+def get_lst_products(url):
+    lst = []
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, 'html.parser')
     liens = soup.find('ol', {'class': 'row'}).find_all('article', {'class': 'product_pod'})
@@ -38,42 +38,42 @@ def books_category_parse_page(url):
         a = lien.find('a')
         link = 'http://books.toscrape.com/catalogue/' + a['href']
         links = link.replace('../../../', '')
-        print(links)
         lst.append(links)
-    if soup.find('li', {'class': 'next'}) != None :
+    if soup.find('li', {'class': 'next'}) is not None:
         np = soup.find('li', {'class': 'next'}).find('a')['href']
-        next_page_url = base_url + np
-        books_category_parse_page(next_page_url)
-    else:
-        ''
+        a = urlparse(url).path
+        b = a.index('/', 26)
+        c = a[:b + 1]
+        next_page_url = 'http://books.toscrape.com' + c + np
+        data = get_lst_products(next_page_url)
+        for d in data:
+            lst.append(d)
+    return lst
 
 
-def extract_book_infos_from_url(url):
-    reponse = requests.get(url)
-    soup = BeautifulSoup(reponse.text, 'html.parser')
+def get_product_data(url):
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'html.parser')
     td = soup.find_all('td')
     product_page_url = url
     universal_product_code = td[0].text
     title = soup.find('h1').text
-    price_including_tax = (td[2].text).replace('Â', '')
-    price_excluding_tax = (td[3].text).replace('Â', '')
-    number_available = (td[5].text).replace('In stock', '').replace('(', '').replace('available)', '')
+    price_including_tax = td[2].text.replace('Â', '')
+    price_excluding_tax = td[3].text.replace('Â', '')
+    number_available = td[5].text.replace('In stock', '').replace('(', '').replace('available)', '')
     product_description = soup.find_all('p')[3].text
     category = str.strip(soup.find_all('li')[2].text)
     review_rating = soup.find("p", "star-rating")["class"][-1]
-    image_url = 'http://books.toscrape.com/' + soup.find("img")["src"]
-    book_info = {'product_page_url': product_page_url,
-            'universal_product_code': universal_product_code,
-            'title': title,
-            'price_including_tax': price_including_tax,
-            'price_excluding_tax': price_excluding_tax,
-            'number_available': number_available,
-            'product_description': product_description,
-            'category': category,
-            'review_rating': review_rating,
-            'image_url': image_url}
+    image_url = 'http://books.toscrape.com/' + soup.find("img")["src"].replace('../../', '')
+    book_info = {product_page_url,
+                 universal_product_code,
+                 title,
+                 price_including_tax,
+                 price_excluding_tax,
+                 number_available,
+                 product_description,
+                 category,
+                 review_rating,
+                 image_url}
     return book_info
-
-
-livre = extract_book_infos_from_url('http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
-print(livre)
